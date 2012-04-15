d2820ceb369fc72fa06ec9518f071f9e4d37e848
OliverWS <OliverWS@gmail.com>
2012-04-15 18:31:20 -0400

    Change port

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World Nodejs run on Amazon EC2 by fyhao\n');
-}).listen(80);
+  res.end('Hello World Nodejs run on Amazon EC2 by ollie\n');
+}).listen(8080);
 console.log('Server running on Amazon EC2');
