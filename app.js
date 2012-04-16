5feb86fbdc1d6029bd810a96745c693c4c1b592e
OliverWS <OliverWS@gmail.com>
2012-04-15 22:47:58 -0400

    Fix headers

--- app.js
+++ app.js
@@ -45,9 +45,10 @@ io.configure('production', function(){
 
 
 app.all('/*', function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "URL TO STATIC ASSET");
- res.header("Access-Control-Allow-Headers", "X-Requested-With");
- res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+ res.writeHead({"Access-Control-Allow-Origin": "*",
+ 	"Access-Control-Allow-Headers":"X-Requested-With",
+    "Access-Control-Allow-Methods":"GET, PUT, POST, DELETE"}
+ );
  next();
 });
 
