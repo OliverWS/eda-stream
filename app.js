117ec0a6185e50b0f218a8a8c8002c878b180a70
OliverWS <OliverWS@gmail.com>
2012-04-15 22:26:08 -0400

    asdfa

--- app.js
+++ app.js
@@ -27,13 +27,21 @@ io.configure('production', function(){
   io.enable('browser client etag');
   io.set('log level', 1);
 
-  io.set('transports', [
-    'websocket'
-  , 'flashsocket'
-  , 'htmlfile'
-  , 'xhr-polling'
-  , 'jsonp-polling'
+  io.enable('browser client minification');  // send minified client
+  io.enable('browser client etag');          // apply etag caching logic based on version number
+  io.enable('browser client gzip');          // gzip the file
+  io.set('log level', 1);                    // reduce logging
+  io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+      'websocket'
+    , 'flashsocket'
+    , 'htmlfile'
+    , 'xhr-polling'
+    , 'jsonp-polling'
   ]);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
 });
 
 io.sockets.on('connection', function (socket) {
