c5fc1d98b071128e1157c3df0206d5399fe63303
OliverWS <OliverWS@gmail.com>
2012-04-15 22:48:57 -0400

    sad

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,5 @@
  */
 
 exports.index = function(req, res){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-
   res.render('index', { title: 'Express' });
 };
