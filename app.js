c001ff61cba398c14d36e83e2ba3741455d786c2
OliverWS <OliverWS@gmail.com>
2012-04-15 22:43:51 -0400

    asdf

--- app.js
+++ app.js
@@ -43,9 +43,14 @@ io.configure('production', function(){
   ]);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+
+app.all('/*', function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "URL TO STATIC ASSET");
+ res.header("Access-Control-Allow-Headers", "X-Requested-With");
+ res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+ next();
 });
+
 
 io.sockets.on('connection', function (socket) {
   clients.push(socket);
