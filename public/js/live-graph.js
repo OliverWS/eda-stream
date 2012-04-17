a08570972ffcf32fed6efa7fe4d1123354b640cd
OliverWS <OliverWS@gmail.com>
2012-04-16 23:39:27 -0400

    Fix

--- public/js/live-graph.js
+++ public/js/live-graph.js
@@ -1,12 +1,4 @@
-Object.prototype.clone = function() {
-  var newObj = (this instanceof Array) ? [] : {};
-  for (i in this) {
-    if (i == 'clone') continue;
-    if (this[i] && typeof this[i] == "object") {
-      newObj[i] = this[i].clone();
-    } else newObj[i] = this[i]
-  } return newObj;
-};
+Array.prototype.clone = function() { return this.slice(0); }
 
 
 var Graph = function(opts){
