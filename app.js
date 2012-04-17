184a151d5f6a3528e5d497fbd287d7daa4da0b2d
OliverWS <OliverWS@gmail.com>
2012-04-17 12:18:58 -0400

    isa

--- app.js
+++ app.js
@@ -48,12 +48,11 @@ app.configure(function(){
 
 io.configure('production', function(){
   io.enable('browser client etag');
-  io.set('log level', 1);
 
   io.enable('browser client minification');  // send minified client
   io.enable('browser client etag');          // apply etag caching logic based on version number
   io.enable('browser client gzip');          // gzip the file
-  io.set('log level', 1);                    // reduce logging
+  io.set('log level', 4);                    // reduce logging
   io.set('transports', [                     // enable all transports (optional if you want flashsocket)
       'websocket'
     , 'flashsocket'
