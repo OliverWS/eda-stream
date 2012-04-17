144a06f2558c6d3bb8d615b2aa17dbb0eaad55f7
OliverWS <OliverWS@gmail.com>
2012-04-17 00:29:27 -0400

    AD

--- public/js/live-graph.js
+++ public/js/live-graph.js
@@ -42,7 +42,6 @@ var Graph = function(opts){
 		data.points.push(dp);
 		
 		if(data.points.length > size){
-			console.log("Too many points: " + data.points.length + " > " + size);
 			data.points.shift();
 		}
 		
