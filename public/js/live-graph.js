5ba1e2b034273dfa416b7d3283c0f5453106548c
OliverWS <OliverWS@gmail.com>
2013-07-05 18:18:57 -0400

    sd

--- public/js/live-graph.js
+++ public/js/live-graph.js
@@ -41,7 +41,7 @@ var Graph = function(opts){
 	};
 	
 	var addPoint = function(dp) {
-		if(!(isNaN(dp)) && (dp != undefined) && (dp != "")){
+		if(!(isNaN(dp)){
 			data.points.push(dp);
 		
 			if(data.points.length > size){
